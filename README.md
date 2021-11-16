2fd68dcdda9379ff1f84c767cadcacc678e7c6de
Your Name <you@example.com>
2021-11-16 21:05:43 +0000

    added buttons to html & js

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
 # javascript-game
+
+## Objective 
+The objective of this project was to create a Javascript game using the skills accumluated from Nology week 1- 4 to show a good understanding off: 1) HTML 2) CSS/SCSS 3)JavaScript
+
+## TechStack used 
+
---
+++ main.js
@@ -0,0 +1,15 @@
+// Aim to build a quiz 
+
+
+// a question header that changes everytime the next question is clicked 
+
+// a display screen that changes question & images everytime next question button is pressed
+
+// a timer to answer the question. at which point when it ends, the user can't select an answer
+
+
+
+// link selected HTML elements to java script using queryselects 
+
+ const userBtn = document.querySelector(".userChoice")
+ console.log(userBtn);
